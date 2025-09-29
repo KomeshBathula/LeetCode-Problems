a7a6ae59bc67d90e5c6421fc62bddecc9870e786
Komesh Bathula <komeshbathula@gmail.com>
2025-09-29 16:53:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String str1 = strs[0];
+        String str2 = strs[strs.length - 1];
+        if(str1.length() != str2.length()) return "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ;i<str1.length();i++){
+            if(str1.charAt(i) == str2.charAt(i)) sb.append(str1.charAt(i));
+            else break;
+        }
+        return sb.toString();
+    }
+}
