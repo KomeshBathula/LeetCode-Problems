3c6abab1b759ce4e5105925031b05966ec24bc01
Komesh Bathula <komeshbathula@gmail.com>
2025-10-08 06:32:57 +0530

    Time: 3 ms (69.8%), Space: 43.4 MB (55.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
+        //O(n) for iterating and adding to the string
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
             if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || Character.isDigit(ch)){
@@ -8,6 +9,7 @@ class Solution {
                 sb.append(c);
             }
         }
+        //checking whether palindrome or not using two pointers
         int left = 0, right = sb.length()-1;
         while(left<=right){
             if(sb.charAt(left)!= sb.charAt(right)) return false;
