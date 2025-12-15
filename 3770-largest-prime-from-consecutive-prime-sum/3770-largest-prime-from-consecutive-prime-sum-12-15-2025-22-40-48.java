a4fe8595217b9467700da442c0906ce60c51402d
Komesh Bathula <komeshbathula@gmail.com>
2025-12-15 22:40:49 +0530

    Time: 204 ms (54.89%), Space: 46.4 MB (65.64%) - LeetHub

---
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum-12-15-2025-22-40-48.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int largestPrime(int n) {
+        boolean isPrime[] = new boolean[n+1];
+        for(int i = 0;i<=n;i++)
+            isPrime[i] = true;
+        isPrime[0] = false; isPrime[1] = false;
+        for(int i = 2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j = i*i;j<=n;j+=i)
+                    isPrime[j] = false;
+            }
+        }
+        int sum = 0, answer = 0;
+        for(int i = 2;i <= n;i++) {
+            if (isPrime[i]) {
+                sum += i;
+                if(sum > n) break;
+                if(isPrime[sum]) answer = sum;
+            }
+        }
+        return answer;
+    }
+}
