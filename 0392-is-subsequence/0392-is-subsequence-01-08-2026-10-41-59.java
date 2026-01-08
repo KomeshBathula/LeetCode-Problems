2c709f623bb70c1c6d3fc0a6fa60fc9e418d758c
Komesh Bathula <komeshbathula@gmail.com>
2026-01-08 10:42:00 +0530

    Time: 2 ms (67.24%), Space: 42.7 MB (81.27%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence-01-08-2026-10-41-59.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        int i = 0;
+        for (int j = 0; j < t.length() && i < s.length() ; j ++) {
+            if (s.charAt(i) == t.charAt(j)) i++;
+        }
+        if (i == s.length()) return true;
+        return false;
+    }
+}
