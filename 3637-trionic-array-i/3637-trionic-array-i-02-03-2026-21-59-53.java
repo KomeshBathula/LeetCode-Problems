5db05dcb6b7b88fb62a0a751725307ce101f038e
Komesh Bathula <komeshbathula@gmail.com>
2026-02-03 21:59:54 +0530

    Time: 1 ms (72.27%), Space: 44.9 MB (31.09%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i-02-03-2026-21-59-53.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if (n < 4) return false;
+        int i = 0;
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        if (i == 0) return false;
+        int decStart = i;
+        while (i + 1 < n && nums[i] > nums[i + 1]) i++;
+        if (i == decStart) return false;
+        int inc2start = i;
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        if (i == inc2start) return false;
+        return i == n - 1;
+    }
+}
