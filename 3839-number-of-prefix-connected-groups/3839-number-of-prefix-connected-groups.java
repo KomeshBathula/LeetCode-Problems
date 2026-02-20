7633e5001d856165e8e8ca3120dc8b5a856f09ca
Komesh Bathula <komeshbathula@gmail.com>
2026-02-20 19:23:07 +0530

    Time: 20 ms (98.74%), Space: 49.1 MB (96.95%) - LeetHub

---
+++ 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int prefixConnected(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() >= k) {
+                String pref = words[i].substring(0, k);
+                map.put(pref, map.getOrDefault(pref, 0) + 1);
+            }
+        }
+        int count = 0;
+        for (int val : map.values()) {
+            if (val >= 2) 
+                count++;
+        }
+        return count;
+    }
+}
