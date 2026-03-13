2e93e98d24b5189a7dfcd812ffa242c1816e678c
Komesh Bathula <komeshbathula@gmail.com>
2026-03-13 21:33:06 +0530

    Time: 12 ms (30.76%), Space: 59.6 MB (42.72%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode middle = findMiddleNode(head);
+        ListNode leftHead = head, rightHead = middle.next;
+        middle.next = null;
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+        return merge(leftHead, rightHead);
+    }
+    public static ListNode findMiddleNode(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
+            }
+            else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return dummyNode.next;
+    }
+}
