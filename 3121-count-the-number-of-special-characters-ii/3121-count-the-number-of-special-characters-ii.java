d7d9603caa0650e85d623a47dac25790827868ae
Komesh Bathula <komeshbathula@gmail.com>
2026-05-27 14:48:14 +0530

    Time: 127 ms (5.37%), Space: 48.2 MB (48.35%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -1,5 +1,7 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
+        /*
+
         HashMap<Character, Integer> map = new HashMap<>();
         // store all occurences overridden by 
         for (int i = 0; i < word.length(); i++) {
@@ -28,5 +30,34 @@ class Solution {
         }
 
         return countOfSpecialChars;
+        */
+        int lowercase[]=new int[26];
+        int uppercase[]=new int[26];
+        Arrays.fill(lowercase,-1);
+        Arrays.fill(uppercase,-1);
+        int count=0;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch=word.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                lowercase[ch-'a']=i;
+            }
+            else{
+                int index= ch-'A';
+                if(uppercase[index]==-1)
+                {
+                    uppercase[index]=i;
+                }
+            }
+        }  
+        for(int i=0;i<26;i++)
+        {
+            if(lowercase[i]!=-1 && uppercase[i]!=-1 && lowercase[i]<uppercase[i])
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
