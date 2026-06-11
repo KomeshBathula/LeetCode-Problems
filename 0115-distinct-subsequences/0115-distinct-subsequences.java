0ed6264e061944c5087267133c97ca889f0f151e
Komesh Bathula <komeshbathula@gmail.com>
2026-06-11 21:49:59 +0530

    Time: 21 ms (35.63%), Space: 54.2 MB (73.38%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -17,11 +17,37 @@ class Solution {
         int m = t.length();
 
         // memoization
+        /*
+
         int[][] dp = new int[n][m];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
 
         return func(n - 1, m - 1, s, t, dp);
+        */
+
+
+        // tabulation
+
+        int[][] dp = new int[n + 1][m + 1];
+        
+        /* Initialize the first row: empty string t can
+        be matched with any non-empty s in one way*/
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= m; i++) dp[0][i] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+
     }
 }
