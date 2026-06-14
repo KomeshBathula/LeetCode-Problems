0dc87d79d0108e1e30172328665e74a6d05d82d6
Komesh Bathula <komeshbathula@gmail.com>
2026-06-14 19:36:20 +0530

    Time: 2337 ms (22.77%), Space: 42.9 MB (72.25%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    private boolean isPalindrome(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+
+            i++; j--;
+        }
+
+        return true;
+    }
+
+    private int func(int i, String s, int[] dp) {
+        if (i == s.length()) return 0;
+
+        if (dp[i] != -1) return dp[i];
+        int min = (int)(1e9);
+
+        for (int j = i; j < s.length(); j++) {
+            if (isPalindrome(i, j, s)) {
+                int cost = 1 + func(j + 1, s, dp);
+                min = Math.min(min, cost);
+            }
+        }
+
+        return dp[i] = min;
+    }
+
+    public int minCut(String s) {
+        // memoization
+        int n = s.length();
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return func(0, s, dp) - 1;
+    }
+}
