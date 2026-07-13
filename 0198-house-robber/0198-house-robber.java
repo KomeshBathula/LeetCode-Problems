309b4624090b3058c28e0da5b6f81c9d7527721c
Komesh Bathula <komeshbathula@gmail.com>
2026-07-13 18:49:28 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -13,10 +13,25 @@ class Solution {
         return dp[i] = Math.max(pick, nonPick);
     }
     public int rob(int[] nums) {
-        int n = nums.length - 1;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
+        int n = nums.length;
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
 
-        return helper(n, nums, dp);
+        // return helper(n, nums, dp);
+
+        // tabulation
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if (i > 1) 
+                pick += dp[i - 2];
+            int nonPick = dp[i - 1];
+
+            dp[i] = Math.max(pick, nonPick);
+        }
+
+        return dp[n - 1];
     }
 }
