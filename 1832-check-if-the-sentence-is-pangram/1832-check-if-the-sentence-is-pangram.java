186e7c297caba7470e707933b8b5b191bd82f3aa
Komesh Bathula <komeshbathula@gmail.com>
2026-07-24 23:37:00 +0530

    Time: 0 ms (100%), Space: 42.9 MB (52.32%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,23 +1,31 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
 
-        if(sentence.length() < 26) 
-            return false;
+        // if(sentence.length() < 26) 
+        //     return false;
 
-        sentence = sentence.toLowerCase();
+        // sentence = sentence.toLowerCase();
 
-        int pan[] = new int[26];
-        for (int i = 0; i < sentence.length(); i++) {
-            char ch = sentence.charAt(i);
-            if(ch <= 'z' && ch >= 'a') 
-                pan[ch - 'a']++;
-        }
+        // int pan[] = new int[26];
+        // for (int i = 0; i < sentence.length(); i++) {
+        //     char ch = sentence.charAt(i);
+        //     if(ch <= 'z' && ch >= 'a') 
+        //         pan[ch - 'a']++;
+        // }
 
-        for (int i = 0; i < 26; i++) {
-            if (pan[i] == 0) 
-                return false;
-        }
+        // for (int i = 0; i < 26; i++) {
+        //     if (pan[i] == 0) 
+        //         return false;
+        // }
         
+        // return true;
+
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (sentence.indexOf(ch) == -1) {
+                return false;
+            }
+        }
         return true;
     }
 }
